db4b7af2e59701820f4357c2cdd5bbf379704c98
aya hussein <aya.hussein3107@gmail.com>
2025-10-18 20:36:18 +0300

    flattening a JS object

---
+++ object-flattening/flatten-object.js
@@ -0,0 +1,21 @@
+let flattenObject = (obj) => {
+    let flattenedObj = {};
+    for (let key of Object.keys(obj)) {
+        if (Object.prototype.toString.call(obj[key]) === '[object Object]') {
+            const internalObj = flattenObject(obj[key]);
+            for (let k of Object.keys(internalObj)) {
+                flattenedObj[key + '.' + k] = internalObj[k]
+            }
+        } else {
+            flattenedObj[key] = obj[key]
+        }
+    }
+    return flattenedObj;
+}
+
+console.log("flattenedObj 1 =",
+    flattenObject({
+        a: { b: { c: 1 }, m: { e: 8 } },
+        f: { g: 1 }
+    })
+);
