ea0fd072751eeb83b6f3bd839ce75d920b8dedbe
aya hussein <aya.hussein3107@gmail.com>
2025-11-02 20:03:20 +0200

    hints and rev string words

---
+++ hints.js
@@ -0,0 +1,5 @@
+    // move first element to end
+    arr.push(arr.shift());
+
+    // sort string by converting to arr
+    let sortedString = originalString.split('').sort().join('');
---
+++ rev-str-words.js
@@ -0,0 +1,18 @@
+function revStr(string) {
+    let result = ''
+    const words = string.split(' ');
+    console.log(words)
+
+    for (let i = words.length - 1; i >= 0; i--) {
+        // console.log(words[i])
+        result += words[i];
+        result += ' ';
+    }
+    return result
+}
+
+console.log("o/p for Aya Hussein =====>", revStr("Aya Hussein Ali"));
+
+
+
+//   -------------------------------- 111###
